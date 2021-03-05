1aca6f2a9f8b877d4a5683a963cf6a0939c21347
gzhanl <gzlianghan@163.com>
2021-03-05 15:14:09 +0800

    20210305_01

--- app.py
+++ app.py
@@ -32,10 +32,14 @@ def main():
     # left siderbar tag
     menu=['板块资金分析','北向资金分析','个股估值']
 
-    choice=st.sidebar.selectbox('Menu',menu)
+    choice=st.sidebar.selectbox('Menu',menu)   # 根据 menu 选择显示页面内容
 
 
-    # 根据 menu 选择显示页面内容
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #                            总体板块 资金分析页面
+    # ------------------------------------------------------------------------------------------------------------------
+
     if choice == '板块资金分析':
         # st.title('板块分析')
         st.header('板块资金分析')
@@ -65,15 +69,23 @@ def main():
              display_bk_stock_cf_data(bk_code)
 
 
+
+
+    #-------------------------------------------------------------------------------------------------------------------
+    #                                 北向资金 板块分析页面
+    #-------------------------------------------------------------------------------------------------------------------
+
     elif choice == '北向资金分析':
 
         #st.header('北向资金分析')
 
-        choice_2=st.sidebar.radio('北向资金',['总体分析','板块分析','个股分析'])
+        choice_2=st.sidebar.radio('北向资金',['总体分析','板块分析','个股分析'])   # 二级菜单---------------
 
 
+        #---------------------------------------------------------------------------------------------------------------
+        #                                北向资金 --- 总体分析页面   二级菜单
+        #---------------------------------------------------------------------------------------------------------------
 
-        # 二级菜单---------------
         if choice_2 == '总体分析':
             st.header('北向资金分析')
             st.subheader('北向资金实时数据  ')
@@ -97,38 +109,71 @@ def main():
                  st.dataframe(df.tail(10))
                  # st.line_chart(df.tail(10)['Date'],float(df.tail(10)['north_flow']))
 
-            with st.beta_expander("Data : 北向资金行业板块情況：" ):
+            with st.beta_expander("Data : 北向资金行业板块最新情況：" ):
+
+                 if st.checkbox("Show Data"):
+
+                     df=ts.get_nbfbk_status()
+
+                     # 保留14位小数位
+                     getcontext().prec = 14
+                     df['td_nf_ratio'] = df['td_nf_ratio'].apply(convert_to_float)
+                     df["td_nf_ratio"] = pd.to_numeric(df["td_nf_ratio"])
+
+                     df["td_increase_nf_ratio"] = pd.to_numeric(df["td_increase_nf_ratio"])
+                    # 保留10位小数位
+                     getcontext().prec = 10
+
+                     # df['td_increase_nf_ratio'] = df['td_increase_nf_ratio'].apply(convert_to_float)
 
 
-                 df=ts.get_nbfbk_status()
 
-                 # 保留14位小数位
-                 getcontext().prec = 14
-                 df['td_nf_ratio'] = df['td_nf_ratio'].apply(convert_to_float)
+                     st.header(df.iloc[0, 0] + '    北向资金行业板块情況  -- 以今日北资增持市值排序（td_increase_shareholding_value） ')  # df.iloc[0, 0] 时间
+
+                     # st.info("td_stock_count:北资今日持股股票只数   td_shareholding_value：北资今日持股市值  td_bk_ratio：北资今日持股占板块比  td_nf_ratio：北资今日持股占北向资金比")
+
+                     st.dataframe(data=df,width=2000, height=500)
+
+            with st.beta_expander("Data : 北向资金行业板块趋势："):
+                 if st.checkbox("Plot Line Chart"):
+                     bk_index = json_to_str()
+                    # 将 str格式板块资料 转为 list
+                     bk_list = list(bk_index['板块'])  # bk_list 全部板块名
+
+                     dfs = {}
+                     df_list = []
+                     # bk_code = bk_index['板块'][bk_choice]
+
+                     for bk_name in bk_list:
+                          try :
+                              bk_code = bk_index['板块'][bk_name]
+                              bk_code_no = bk_code[-3:]
+                              # df=ts.get_nbfbk_hist_capital_flow(bk_code_no)
+                              dfs['df_{}'.format(bk_code_no)] = ts.get_nbfbk_hist_capital_flow(bk_code_no)
+                              df_list.append(dfs['df_{}'.format(bk_code_no)])
+                              print(df_list)
+                          except Exception as e:
+                              pass
+                          continue
+                     df = pd.concat(df_list)
+                     df.dropna(axis=0, how='any', inplace=True)
+
+                     File_path = 'C:\\Users\\DELL\\Desktop\\Data_Web\\Data\\'
+                     # df.to_excel(File_path.strip() + 'nfbk_trend.xlsx')
+                     pd.read_excel(File_path.strip() + 'nfbk_trend.xlsx')
+
+                     st.success("Finish ! ")
 
 
-                # 保留10位小数位
-                 getcontext().prec = 10
-
-                 # df['td_increase_nf_ratio'] = df['td_increase_nf_ratio'].apply(convert_to_float)
-
-
-
-                 st.header(df.iloc[0, 0] + '    北向资金行业板块情況  -- 以今日北资增持市值排序（td_increase_shareholding_value） ')  # df.iloc[0, 0] 时间
-
-                 # st.info("td_stock_count:北资今日持股股票只数   td_shareholding_value：北资今日持股市值  td_bk_ratio：北资今日持股占板块比  td_nf_ratio：北资今日持股占北向资金比")
-
-                 st.dataframe(df)
+      # ---------------------------------------------------------------------------------------------------------------
+      #                                北向资金 --- 板块分析页面
+      # ---------------------------------------------------------------------------------------------------------------
 
         elif choice_2 == '板块分析':
             st.header('北向资金分析')
             st.subheader('北向资金板块持股历史数据')
 
             bk_code = select_bk_item()
-
-
-
-
 
 
             # 显示数据
@@ -144,7 +189,9 @@ def main():
 
                 plot_nbf_total_bk_data_LineChart(bk_code)
 
-
+      # ---------------------------------------------------------------------------------------------------------------
+      #                                北向资金 --- 个股资金分析页面
+      # ---------------------------------------------------------------------------------------------------------------
         elif choice_2 == '个股分析':
                 st.subheader('北向资金个股资金流')
                 col1, col2 = st.beta_columns([1, 5])
@@ -152,7 +199,7 @@ def main():
                 with col1:
 
 
-                    stock_no = st.text_input("输入A股票号码 Enter    ", max_chars=10)
+                    stock_no = st.text_input("输入A股代码/沪深港代码 Enter", max_chars=10)
 
                 # with col2:
                 #     st.subheader('  ')
@@ -161,9 +208,13 @@ def main():
                     st.info('Please enter no.')
                 elif stock_no != '':
 
-                    
-                    stock_no=Trans_HKCODE_to_ACODE(stock_no)
-                    print(stock_no)
+
+                    # 如果是 A 股 代码 就转换为TS代码
+                    if len(stock_no)== 6 :
+                        stock_no=Trans_HKCODE_to_ACODE(stock_no)
+                        # print(stock_no)
+
+
                     # df_stock = pd.read_csv('C:\\Users\\DELL\\Desktop\\Data_Web\\沪深股通持股记录多日.csv')
                     df_stock=ts.get_All_Stock_HK_Shareholding_Hist_CSV()
                     df_stock = df_stock[df_stock['Stock_Code'] == int(stock_no)]
@@ -237,8 +288,8 @@ def main():
 
 
 
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # 读取json 档资料 转换为 str
@@ -354,7 +405,7 @@ def plot_bk_buy_Ratio_LineChart(bk_code):
     # print(type(df["date"]))  #  <class 'pandas.core.series.Series'>
     #  画图 行业历史资金流
     # df["mainb_ratio_flow"]=df["mainb_ratio"]
-    print(type(df['mainb_ratio']))
+    # print(type(df['mainb_ratio']))
     # print(type(df['mainb_ratio_flow']))
     df['mainb_ratio_flow']=pd.to_numeric(df['mainb_ratio'], errors='coerce')
     df['Slargeb_ratio_flow'] = pd.to_numeric(df['Slargeb_ratio'], errors='coerce')
@@ -370,8 +421,8 @@ def plot_bk_buy_Ratio_LineChart(bk_code):
     # print(df['mainb_ratio_flow'])
     # print(df['M_SL_buy_ratio_flow'])
     s = pd.Series([2, np.nan, 5, -1, 0])
-    print(type(s))
-    print(s.cumsum())
+    # print(type(s))
+    # print(s.cumsum())
 
 
     Line_Chart = {
@@ -429,6 +480,18 @@ def plot_bk_buy_Ratio_LineChart(bk_code):
 # 显示 板块个股资金流 资料
 def display_bk_stock_cf_data(bk_code):
     df=ts.get_bk_stock_capital_flow(bk_code)
+
+    df['Price'].loc[df['Price']=='-']=0    # 如果该列 数据 为 ‘-’ ，则改为 0
+
+    df["Price"] = pd.to_numeric(df["Price"])
+
+
+    # if \
+    #         df["Price"] == "-":
+    #     df["Price"]=0
+    # else:
+    #     df["Price"] = pd.to_numeric(df["Price"])
+    # df.fillna(value=0)
     st.dataframe(data=df,width=2000, height=2000)
 
 
@@ -443,14 +506,14 @@ def display_nbfbk_data(bk_code):
     st.dataframe(df)
 
 
-#  画图 当前板块北向资金占总北向资金比
+#  画图 当前板块北向资金占总 北向 资金比
 def plot_nbfbk_data_LineChart(bk_code):
     bk_code_no = bk_code[-3:]
     # print(bk_code_no)
     df = ts.get_nbfbk_hist_capital_flow(bk_code_no)
 
     df = df.drop(index=[0])  # 去除 None 值 ，不然 max() 失效
-    print(df)
+    # print(df)
     col1, col2, col3 = st.beta_columns([1, 1, 1])
 
     min_value = df["znzjb"].astype(str).min()
@@ -458,12 +521,12 @@ def plot_nbfbk_data_LineChart(bk_code):
     min_value = round(float(min_value) * 0.97 ,4 )
 
 
-    print(min_value)
+    # print(min_value)
 
     max_value = df["znzjb"].astype(str).max(skipna=True)
     max_value = round(float(max_value) * 1.03 , 4)
 
-    print(max_value)
+    # print(max_value)
 
     # with col1:
     #     min_scale = st.slider('MIN', 0.000, 1.000, min_value, 0.005)
@@ -529,7 +592,7 @@ def plot_nbf_total_bk_data_LineChart(bk_code):
     df = ts.get_nbfbk_hist_capital_flow(bk_code_no)
 
     df = df.drop(index=[0])  # 去除 None 值 ，不然 max() 失效
-    print(df)
+    # print(df)
     col1, col2, col3 = st.beta_columns([1, 1, 1])
 
     min_value = df["cgbkb"].astype(str).min()
@@ -537,12 +600,12 @@ def plot_nbf_total_bk_data_LineChart(bk_code):
     min_value = round(float(min_value) * 0.97 ,4 )
 
 
-    print(min_value)
+    # print(min_value)
 
     max_value = df["cgbkb"].astype(str).max(skipna=True)
     max_value = round(float(max_value) * 1.03 , 4)
 
-    print(max_value)
+    # print(max_value)
 
     # with col1:
     #     min_scale = st.slider('MIN', 0.000, 1.000, min_value, 0.005)
@@ -609,20 +672,20 @@ def plot_stock_in_nbf_Shareholding_Percent_LineChart(df_stock):
 
     Stock_Name = df_stock.iloc[1,3]  # 獲取第第3列 第1個數
     # df.iloc[0, 0]
-    print(Stock_Name)
+    # print(Stock_Name)
     # min_value = df_stock["Shareholding_Percent"].astype(str).min()
     min_value = df_stock["Shareholding_Percent"].min()
 
     # min_value = float(min_value) * 0.97
     min_value = round(float(min_value) * 0.97 ,2 )
 
-    print(min_value)
+    # print(min_value)
 
     # max_value = df_stock["Shareholding_Percent"].astype(str).max(skipna=True)
     max_value = df_stock["Shareholding_Percent"].max()
     max_value = round(float(max_value) * 1.03 , 2)
 
-    print(max_value)
+    # print(max_value)
 
     # with col1:
     #     min_scale = st.slider('MIN', 0.000, 1.000, min_value, 0.005)
@@ -693,6 +756,8 @@ def display_Stock_HK_Shareholding_Today():
 
     df["Shareholding_Percent"] = pd.to_numeric(df["Shareholding_Percent"])
     df = df.sort_values('Shareholding_Percent',ascending=False)
+
+
     # st.dataframe(df)
     st.subheader('持股日期：' + Date_Shareholding )
     st.dataframe(data=df, height=1000)
